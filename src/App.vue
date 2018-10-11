3d851f4fb197130fbf64e2cfb68c12ab974ccc5b
Margarita Deinekina <margarita.deinekina@khk.ee>
2018-10-11 14:01:00 +0300

    Loodud numbrifaktide aplikatsioon

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Faktid />
+  </div>
+</template>
+
+<script>
+import Faktid from './components/Faktid.vue'
+
+export default {
+  name: 'app',
+  components: {
+   Faktid
+  }
+}
+</script>
+
+<style>
+</style>
